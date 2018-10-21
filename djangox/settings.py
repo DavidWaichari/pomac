995072fe6f8df04d4c0c4fa187ca08b81dcd9a33
David <dwaichari@gmail.com>
2018-10-21 12:34:43 +0300

    rectify date sweetify for dates validation

--- djangox/settings.py
+++ djangox/settings.py
@@ -95,6 +95,17 @@ WSGI_APPLICATION = 'djangox.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'pomac',
+#          'USER': 'postgres',
+#          'PASSWORD': 'root',
+#          'HOST': '127.0.0.1',
+#          'PORT': '5432',
+#      }
+# }
+
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
