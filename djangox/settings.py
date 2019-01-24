9e990d4ff8cb90ebe2ccfac7b926fdd2d7a93f8a
David Waichari <dwaichari@gmail.com>
2019-01-24 08:50:00 +0300

    remove my awaiting schedule for hearing

--- djangox/settings.py
+++ djangox/settings.py
@@ -170,7 +170,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dwaichari@gmail.com'
+EMAIL_HOST_USER = 'petitionsmaster.com'
 EMAIL_HOST_PASSWORD = 'PASSnimimi003'
 
 AUTHENTICATION_BACKENDS = (
