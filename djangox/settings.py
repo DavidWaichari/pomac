c2987e295544cdbc1ebe8f537400b81b59f26abb
David Waichari <dwaichari@gmail.com>
2019-01-31 11:56:22 +0300

    use debug = false on production

--- djangox/settings.py
+++ djangox/settings.py
@@ -214,3 +214,35 @@ ACCOUNT_SESSION_REMEMBER = None
 django_heroku.settings(locals())
 
 USE_TZ = False
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+     'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
